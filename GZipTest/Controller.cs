3344c33c74cbcdd9f34bbd69aabc2c5f079a6e45
hexmar <evo118@outlook.com>
2017-11-06 22:46:48 +0300

    Main method separation

--- GZipTest/Controller.cs
+++ GZipTest/Controller.cs
@@ -6,47 +6,6 @@ namespace GZipTest
 {
     public class Controller
     {
-        static void Main(string[] args)
-        {
-            if (!ValidArgs(args))
-            {
-                Console.WriteLine("Invalid arguments. Use: GZipTest.exe <compress/decompress> <Input file> <Output file>");
-                return;
-            }
-            Controller controller;
-            try
-            {
-                controller = new Controller(args[1], args[2]);
-            }
-            catch (Exception)
-            {
-                return;
-            }
-            try
-            {
-                if (args[0].Equals("compress"))
-                    controller.Compress();
-                else
-                    controller.Decompress();
-            }
-            catch (Exception e)
-            {
-                Console.Write("Error! " + e.Message);
-                File.Delete(args[2]);
-                return;
-            }
-            Console.WriteLine("Done");
-        }
-
-        private static bool ValidArgs(string[] args)
-        {
-            if (args.Length != 3)
-                return false;
-            if (!args[0].Equals("compress") && !args[0].Equals("decompress"))
-                return false;
-            return true;
-        }
-
         static int threadCount = Environment.ProcessorCount;
 
         const int dataBlockSize = 0x100000;
